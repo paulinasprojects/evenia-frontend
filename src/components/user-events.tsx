286c11d2bf5af4282bef02f88964d97fa6dec061
paulinasprojects <virtualpaula93@gmail.com>
2024-09-03 16:42:08 +0300

    added empty state of the user profile page if you don't have any events

--- src/components/user-events.tsx
+++ src/components/user-events.tsx
@@ -8,48 +8,59 @@ import EventCard from "./event-card";
 import "@/styles/user-events.scss";
 
 const UserEvents = () => {
-
+  const events = false;
   const navigate = useNavigate();
 
   return (
-    <div className="user-events-container">
-      <div className="user-list-events-title-container">
-        <h1 className="user-list-events-title">User List Events</h1>
-      </div>
-      <div className="user-list-events-card-container">
-        <EventCard
-          onClick={() => navigate("/detail/1")} 
-          image={eventImage} 
-          date="Mon, Oct 25, 8 : 30 AM" 
-          title="Critical Thinking Skills For All Profesional" 
-          author="Jalu Detya"
-        />
-          <EventCard
-          onClick={() => navigate("/detail/1")} 
-          image={eventImageTwo} 
-          date="Mon, Oct 25, 8 : 30 AM" 
-          title="Critical Thinking Skills For All Profesional" 
-          author="Jalu Detya"
-        />
-        <EventCard
-          onClick={() => navigate("/detail/1")} 
-          image={eventImageThree} 
-          date="Mon, Oct 25, 8 : 30 AM" 
-          title="Critical Thinking Skills For All Profesional" 
-          author="Jalu Detya"
-        />
-        <EventCard
-          onClick={() => navigate("/detail/1")} 
-          image={eventImageFour} 
-          date="Mon, Oct 25, 8 : 30 AM" 
-          title="Critical Thinking Skills For All Profesional" 
-          author="Jalu Detya"
-        />
-      </div>
-        <div className="user-list-event-button-container">
-          <button className="see-more-button">See More</button>
+    <>
+      { events ? (
+        <div className="user-events-container">
+        <div className="user-list-events-title-container">
+          <h1 className="user-list-events-title">User List Events</h1>
         </div>
-    </div>
+        <div className="user-list-events-card-container">
+          <EventCard
+            onClick={() => navigate("/detail/1")} 
+            image={eventImage} 
+            date="Mon, Oct 25, 8 : 30 AM" 
+            title="Critical Thinking Skills For All Profesional" 
+            author="Jalu Detya"
+          />
+            <EventCard
+            onClick={() => navigate("/detail/1")} 
+            image={eventImageTwo} 
+            date="Mon, Oct 25, 8 : 30 AM" 
+            title="Critical Thinking Skills For All Profesional" 
+            author="Jalu Detya"
+          />
+          <EventCard
+            onClick={() => navigate("/detail/1")} 
+            image={eventImageThree} 
+            date="Mon, Oct 25, 8 : 30 AM" 
+            title="Critical Thinking Skills For All Profesional" 
+            author="Jalu Detya"
+          />
+          <EventCard
+            onClick={() => navigate("/detail/1")} 
+            image={eventImageFour} 
+            date="Mon, Oct 25, 8 : 30 AM" 
+            title="Critical Thinking Skills For All Profesional" 
+            author="Jalu Detya"
+          />
+        </div>
+          <div className="user-list-event-button-container">
+            <button className="see-more-button">See More</button>
+          </div>
+        </div> 
+      ) : (
+        <div className="user-no-events-container">
+          <div className="user-no-events-title-container">
+            <h1 className="user-no-events-title">No Events Yet</h1>
+            <span className="user-no-events-description">The organizer don't have any events yet. Check back later!</span>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
